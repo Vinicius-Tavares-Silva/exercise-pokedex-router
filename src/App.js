94497cf59ab589168edece929a122b4057bc1383
Vinicius Tavares <vini_tavares10@hotmail.com>
2021-07-05 18:14:37 -0300

    Exe 1 - Constroi pagina inicial utilizando Router

--- src/App.js
+++ src/App.js
@@ -2,12 +2,20 @@ import React from 'react';
 import './App.css';
 import pokemons from './data';
 import Pokedex from './Pokedex';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
       <h1> Pokedex </h1>
-      <Pokedex pokemons={pokemons} />
+      <BrowserRouter>
+        <Switch>
+          <Route
+            path="/"
+            render={(props) => <Pokedex {...props} pokemons={pokemons} />}
+          />
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
